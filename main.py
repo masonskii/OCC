d5346042eeb819e91736c89b46198e83865c0d71
masonskiy <gavrin.01@bk.ru>
2023-09-15 02:01:34 +0300

    iss1

---
+++ main.py
@@ -0,0 +1,106 @@
+import subprocess
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/runcode', methods=['POST'])
+def run_code():
+    code = request.json['code']
+    language = request.json['language']
+
+    # If language is Python, use the same logic as before
+    if language == 'python':
+        try:
+            # Execute the code
+            exec_globals = {}
+            exec_locals = {'output': ''}
+            exec(code, exec_globals, exec_locals)
+            output = exec_locals.get('output', '')
+            error = None
+        except Exception as e:
+            output = ''
+            error = str(e)
+
+        result = {
+            'output': output,
+            'error': error
+        }
+
+    # If language is C++, compile and execute the code
+    elif language == 'cpp':
+        try:
+            # Write the code to a file
+            with open('builds/cpp/code.cpp', 'w') as file:
+                file.write(code)
+
+            # Compile the code
+            compile_cmd = ['g++', 'builds/cpp/code.cpp', '-o', 'builds/cpp/code']
+            subprocess.run(compile_cmd, check=True)
+
+            # Execute the compiled code
+            execute_cmd = ['builds/cpp/code']
+            process = subprocess.run(execute_cmd, capture_output=True, text=True)
+
+            # Get the output and error (if any)
+            output = process.stdout.strip()
+            error = process.stderr.strip() or None
+        except subprocess.CalledProcessError as e:
+            output = ''
+            error = e.stderr.strip() or str(e)
+        except Exception as e:
+            output = ''
+            error = str(e)
+
+        result = {
+            'output': output,
+            'error': error
+        }
+
+    # If language is Java, compile and execute the code
+    elif language == 'java':
+        try:
+            # Write the code to a file
+            with open('Main.java', 'w') as file:
+                file.write(code)
+
+            # Compile the code
+            compile_cmd = ['javac', './Main.java']
+            subprocess.run(compile_cmd, check=True)
+
+            # Execute the compiled code
+            execute_cmd = ['java', 'Main']
+            process = subprocess.run(execute_cmd, capture_output=True, text=True)
+
+            # Get the output and error (if any)
+            output = process.stdout.strip()
+            error = process.stderr.strip() or None
+        except subprocess.CalledProcessError as e:
+            output = ''
+            error = e.stderr.strip() or str(e)
+        except Exception as e:
+            output = ''
+            error = str(e)
+
+        result = {
+            'output': output,
+            'error': error
+        }
+    else:
+        result = {
+            'output': '',
+            'error': 'Unsupported language'
+        }
+
+    return jsonify(result)
+
+
+
+
+if __name__ == '__main__':
+    app.run()
